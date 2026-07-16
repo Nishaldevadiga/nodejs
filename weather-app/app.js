06a6b1450efd88edbed7f25dc957d19ab0d033f6
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2026-07-16 19:54:54 -0400

    "changes"

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,8 +3,13 @@ import request from "request";
 
 //https://api.weatherstack.com/current?access_key=240dc3cf8d5c17af59461186bfe66513&query=42.2626,-71.8023
 
-const url='https://api.weatherstack.com/current?access_key=240dc3cf8d5c17af59461186bfe66513&query=42.2626,-71.8023';
+const url = 'https://api.weatherstack.com/current?access_key=240dc3cf8d5c17af59461186bfe66513&query=42.2626,-71.8023';
 
-request({url,json:true},(err,res)=>{
-console.log(res.body.current);
+request({ url, json: true }, (err, res) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(res.body.current);
+    }
 })
+
