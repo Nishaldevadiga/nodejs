94fa60cfddc96a7dcadd5152e402d3913907bfc6
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2026-07-17 08:27:35 -0400

    "nodemon"

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,12 +1,21 @@
 import express from 'express';
 
-const app=express();
+const app=express()
 
 app.get('',(req,res)=>{
    res.send('Hello express!')
 })
 
+app.get('/help',(req,res)=>{
+   res.send('Help!')
+})
+
+
 
 //app.com
 //app.com/help
 //app.com/about
+
+app.listen(3000,()=>{
+    console.log('server started...');
+})
