15ec1de41f2687f288d90a01bdaeca837a69e95f
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2026-07-17 21:41:06 -0400

    "changes"

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import hbs from 'hbs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,10 +11,26 @@ console.log(__dirname)
 console.log(path.join(__dirname, '../public'))
 
 const app = express()
+const publicDirectoryPath=path.join(__dirname, '../public')
+const viewsPath=path.join(__dirname, '../templates')
 
 app.set('view engine','hbs')
+app.set('views',viewsPath)
+app.use(express.static(publicDirectoryPath))
 
-app.use(express.static(path.join(__dirname, '../public')))
+app.get('',(req,res)=>{
+    res.render('index',{
+        title:"hbs content",
+        name:"nishal sukumar"
+    });
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about',{
+         title:"About",
+        name:"nishal sukumar"
+    })
+})
 
 
 //app.com
