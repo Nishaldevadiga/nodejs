d27d2a0dbd19a2baf3f7ec9cc0825320c0e2aac3
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2026-07-17 21:46:37 -0400

    "partials"

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -13,9 +13,13 @@ console.log(path.join(__dirname, '../public'))
 const app = express()
 const publicDirectoryPath=path.join(__dirname, '../public')
 const viewsPath=path.join(__dirname, '../templates/views')
+const partialsPath=path.join(__dirname, '../templates/partials')
 
 app.set('view engine','hbs')
 app.set('views',viewsPath)
+hbs.registerPartials(partialsPath)
+
+
 app.use(express.static(publicDirectoryPath))
 
 app.get('',(req,res)=>{
