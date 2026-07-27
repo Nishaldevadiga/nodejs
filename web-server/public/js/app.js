757be78acb36c035eecea48072c340c938d8b4a4
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2026-07-27 18:15:35 -0400

    "http calls"

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,20 @@
 console.log("client loaded");
+
+
+
+
+
+// fetch('https://dummyjson.com/recipes/1').then((res) => {
+//     res.json().then((data) => {
+//         console.log(data);
+//     })
+// });
+
+
+async function recipe() {
+    const prod = await fetch('https://dummyjson.com/recipes/1');
+    const data = await prod.json();
+    console.log("recipe",data);
+}
+
+recipe();
