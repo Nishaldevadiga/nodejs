b173e4f899b1430d2c06692393050b7c5a18991e
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2026-07-27 17:03:06 -0400

    "changes"

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,6 +4,7 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import hbs from 'hbs';
 
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
