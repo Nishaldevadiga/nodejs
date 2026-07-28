1388c95647bb7e3e59154bbd5014f430c9af9cdf
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2026-07-27 22:27:32 -0400

    "Git integrations"

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,23 +1,23 @@
 async function recipe(id) {
-    try{
-    const prod = await fetch(`https://dummyjson.com/recipes/${id}`);
-    const data = await prod.json();
+    try {
+        const prod = await fetch(`https://dummyjson.com/recipes/${id}`);
+        const data = await prod.json();
 
-    return data;
-    }catch(err){
+        return data;
+    } catch (err) {
         console.log("error occured");
     }
 }
 
-const form=document.querySelector('form');
-const searchEle=document.querySelector('input');
-let messageOne=document.querySelector('#msg1');
-messageOne.textContent='Loading the puzzle';
+const form = document.querySelector('form');
+const searchEle = document.querySelector('input');
+let messageOne = document.querySelector('#msg1');
+messageOne.textContent = 'Loading the puzzle';
 
-form.addEventListener('submit',(e)=>{
+form.addEventListener('submit', (e) => {
     e.preventDefault();
-    const rec=recipe(searchEle.value);
-rec.then(m=>messageOne.textContent=m.name);
+    const rec = recipe(searchEle.value);
+    rec.then(m => messageOne.textContent = m.name);
 })
 
 
