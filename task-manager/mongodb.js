9d8661eb52e2203e678f429d213b7e839e423b83
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2026-07-29 19:56:24 -0400

    "changes"

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -7,6 +7,9 @@ const mongoClient=mongodb.MongoClient;
 const connectionURL ='mongodb://127.0.0.1:27018';
 const database='task-manager';
 
-mongoClient.connect(connectionURL,{useNewUrlParser:true},()=>{
-    console.log("database connected");
+mongoClient.connect(connectionURL,{useNewUrlParser:true},(err,client)=>{
+    if(err){
+        console.log('error');
+    }
+        console.log('success');
 })
