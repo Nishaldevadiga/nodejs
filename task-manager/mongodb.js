651d8d19cc8c86452278e77966e194c7879ead08
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2026-07-30 17:26:13 -0400

    "querying the db"

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -9,24 +9,23 @@ const connectionURL = 'mongodb://127.0.0.1:27018';
 const database = 'task-manager';
 
 const id = new ObjectID();
-console.log("id", id);
-console.log(id.getTimestamp());
 
 async function main() {
     const client = new MongoClient(connectionURL);
 
     try {
         await client.connect();
-        const db = client.db(database);
 
-        db.collection('users').findOne({
-            name: 'nishal'
-        })
-    } catch (err) {
-        console.log(err);
+        const db = client.db(database);
+        const user = (await db.collection('users').find({ age: 49}).toArray()).length;
+
+        console.log(user);
+    } catch (error) {
+        console.log('unable to connect to database or fetch document');
     } finally {
         await client.close();
     }
 }
 
 main();
+
