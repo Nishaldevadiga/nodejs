744a4be0689bad783f26f2838ffaae526d25fc9c
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2026-08-04 23:53:14 -0400

    "validations"

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -4,17 +4,25 @@ await mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api');
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required:true
     },
     age: {
-        type: Number
+        type: Number,
+        validate(value){
+            if(value<0){
+                throw new Error('age cannot be less than 0');
+                
+            }
+        }
     }
 })
 
 
 const me = new User({
-    name: "Nishal",
-    age: 28
+   
+    name:'kiran',
+    age:-2
 })
 
 me.save().then((res)=>{
