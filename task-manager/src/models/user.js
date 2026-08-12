09be21a485f47b9904c996312993570519569e46
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2026-08-12 19:11:50 -0400

    "folder structure"

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -6,17 +6,17 @@ await mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api');
 const User = mongoose.model('User', {
     name: {
         type: String,
-        required:true
+        required: true
     },
-    email:{
-     type:String,
-     required:true,
+    email: {
+        type: String,
+        required: true,
     },
     age: {
         type: Number,
-        default:0,
+        default: 0,
     }
 })
 
-module.exports=User
+module.exports = User
 
